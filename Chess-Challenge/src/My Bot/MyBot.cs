87b0a183e86199639a358464caa76d60818e146d
Bryn Deering <bdeering@uoguelph.ca>
2023-08-04 19:35:17 -0400

    Basic negamax algorithm with alpha beta

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,54 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
+    private Board board;
+    private Timer timer;
+    private List<int> scores = new List<int>();
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        this.board = board;
+        this.timer = timer;
+
+        var alpha = int.MinValue;
+        var beta = int.MaxValue;
+        var score = NegaMax(3, alpha, beta);
+
+        var idx = 0;
+        foreach (var s in scores)
+        {
+            if (s == score) return board.GetLegalMoves()[idx];
+            idx++;
+        }
+
+        return board.GetLegalMoves()[0];
+    }
+
+    private int NegaMax(int depth, int alpha, int beta)
+    {
+        if (depth == 0) { return Eval(); }
+
+        int score;
+        foreach(var move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            score = -NegaMax(0, alpha, beta);
+            board.UndoMove(move);
+
+            if (depth == 0) { scores.Add(score); }
+
+            if (score >= beta) { return beta; }
+            if (score > alpha) { alpha = score; }
+        }
+
+        return alpha;
+    }
+
+    private int Eval()
+    {
+        return 0;
     }
 }
