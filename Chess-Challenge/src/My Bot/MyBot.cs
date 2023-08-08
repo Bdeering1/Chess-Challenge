c6cc9e2acf379bdd577bdf3b7b85240c7f32bb24
Bryn Deering <bdeering@uoguelph.ca>
2023-08-07 20:18:08 -0400

    Fixed negamax algorithm

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,21 +6,35 @@ public class MyBot : IChessBot
 {
     private Board board;
     private Timer timer;
-    private List<int> scores = new List<int>();
+    private List<int> scores;
+
+    private int DEPTH = 1;
 
     public Move Think(Board board, Timer timer)
     {
         this.board = board;
         this.timer = timer;
 
+        scores = new();
         var alpha = int.MinValue;
         var beta = int.MaxValue;
-        var score = NegaMax(3, alpha, beta);
+        var score = NegaMax(DEPTH, alpha, beta);
+
+        Console.WriteLine(scores.Count);
+        foreach (var s in scores)
+        {
+            Console.Write($"{s}, ");
+        }
+        Console.WriteLine();
 
         var idx = 0;
         foreach (var s in scores)
         {
-            if (s == score) return board.GetLegalMoves()[idx];
+            if (s == score)
+            {
+                Console.WriteLine(idx);
+                return board.GetLegalMoves()[idx];
+            }
             idx++;
         }
 
@@ -29,19 +43,19 @@ public class MyBot : IChessBot
 
     private int NegaMax(int depth, int alpha, int beta)
     {
-        if (depth == 0) { return Eval(); }
+        if (depth == 0) return Eval();
 
         int score;
         foreach(var move in board.GetLegalMoves())
         {
             board.MakeMove(move);
-            score = -NegaMax(0, alpha, beta);
+            score = NegaMax(depth - 1, -beta, -alpha);
             board.UndoMove(move);
 
-            if (depth == 0) { scores.Add(score); }
+            if (depth == DEPTH) { scores.Add(score); }
 
-            if (score >= beta) { return beta; }
-            if (score > alpha) { alpha = score; }
+            if (score >= beta) return beta;
+            if (score > alpha) alpha = score;
         }
 
         return alpha;
@@ -49,6 +63,23 @@ public class MyBot : IChessBot
 
     private int Eval()
     {
-        return 0;
+        var score = 0;
+        foreach (var list in board.GetAllPieceLists())
+        {
+            var value = (int)list.TypeOfPieceInList switch
+            {
+                1 => 100,
+                2 or 3 => 325,
+                4 => 550,
+                5 => 1000,
+                _ => 50000,
+            } * list.Count;
+
+            if (!list.IsWhitePieceList) value = -value;
+            score += value;
+        }
+
+        if (board.IsWhiteToMove) score = -score;
+        return score;
     }
 }
